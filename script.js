4b76a42bb52edceaedf83fe3178fffbdca7b9171
= <108730325+keki97@users.noreply.github.com>
2022-07-18 08:35:51 +0200

    No scroll when menu-button is active

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ document
     document
       .querySelector(".nav-list-mobile-view-container")
       .classList.remove("hidden");
+    document.querySelector("html, body").style.overflow = "hidden";
   });
 
 document.querySelector(".close-btn").addEventListener("click", function () {
   document
     .querySelector(".nav-list-mobile-view-container")
     .classList.add("hidden");
+  document.querySelector("html, body").style.overflow = "auto";
 });
